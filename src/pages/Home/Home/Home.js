a717c7652a1b8ea3f89205eed9627cfcc93cb5f3
tareque-bin-habib <shad.tbh@gmail.com>
2023-03-06 20:14:18 +0600

    banner added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -4,6 +4,7 @@ import Categories from '../Categories/Categories';
 import Count from '../Count/Count';
 import MakeDifferent from '../MakeDifferent/MakeDifferent';
 import ProudService from '../ProudService/ProudService/ProudService';
+import SecondBanner from '../SecondBanner/SecondBanner';
 
 const Home = () => {
     return (
@@ -13,6 +14,7 @@ const Home = () => {
             <ProudService></ProudService>
             <MakeDifferent></MakeDifferent>
             <Count></Count>
+            <SecondBanner></SecondBanner>
         </div>
     );
 };
---
+++ src/pages/Home/SecondBanner/SecondBanner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SecondBanner.css'
+
+const SecondBanner = () => {
+    return (
+        <div className='banner'>
+            <div className='flex justify-center items-center banner-content'>
+                <h1 className='text-4xl text-white text-center'>Let your designer find the right pieces for you, <br /> your space and budget. Give us a call or get in touch!</h1>
+            </div>
+        </div>
+    );
+};
+
+export default SecondBanner;
