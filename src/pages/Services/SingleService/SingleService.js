5e5ffacc631573b7ae5e7531f19129de2b4409ad
tareque-bin-habib <shad.tbh@gmail.com>
2023-03-28 16:33:45 +0600

    content aligned

--- src/pages/Services/SingleService/SingleService.js
+++ src/pages/Services/SingleService/SingleService.js
@@ -20,21 +20,23 @@ const SingleService = () => {
                 </div>
             </div>
             <div className='max-w-[1440px] mx-auto py-20'>
-                <div className='grid xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-2    gap-20'>
-                    <div>
-                        <h1 className='text-2xl font-bold'>{desc}</h1 >
-                        <div className='pt-5 flex gap-4'>
-                            <AiOutlineUser className='text-2xl'></AiOutlineUser>
-                            <h1 className='font-bold'>{person}</h1>
-                        </div>
-                        <p className='pt-5'>{mainDesc}</p>
-                        <div className='flex gap-4 pt-5'>
-                            <p className='font-bold text-2xl'><span>Price:</span> {price}$</p>
-                        </div>
+                <div className='grid xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1    gap-20'>
+                    <div className='flex justify-center px-5'>
+                        <div>
+                            <h1 className='text-2xl font-bold'>{desc}</h1 >
+                            <div className='pt-5 flex gap-4'>
+                                <AiOutlineUser className='text-2xl'></AiOutlineUser>
+                                <h1 className='font-bold'>{person}</h1>
+                            </div>
+                            <p className='pt-5'>{mainDesc}</p>
+                            <div className='flex gap-4 pt-5'>
+                                <p className='font-bold text-2xl'><span>Price:</span> {price}$</p>
+                            </div>
 
-                        <label htmlFor="order-modal" onClick={() => setOrder(service)} className="btn rounded-xl bg-pink-500 font-bold mt-5 text-white border-0 ">Please Order</label>
+                            <label htmlFor="order-modal" onClick={() => setOrder(service)} className="btn rounded-xl bg-pink-500 font-bold mt-5 text-white border-0 ">Please Order</label>
+                        </div>
                     </div>
-                    <div>
+                    <div className='flex justify-center px-5'>
                         <img src={image} alt="" />
                     </div>
                 </div>
